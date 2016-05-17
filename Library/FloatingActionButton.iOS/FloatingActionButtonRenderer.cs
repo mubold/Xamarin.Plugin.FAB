a0ac36b2209262acb68268d2c59f4cd821c974b9
Drew Frisk <dfrisk@bhtp.com>
2016-05-16 21:53:57 -0500

    fixed issue with FAB not being clickable on iOS
    closes issue #3

--- Library/FloatingActionButton.iOS/FloatingActionButtonRenderer.cs
+++ Library/FloatingActionButton.iOS/FloatingActionButtonRenderer.cs
@@ -9,104 +9,84 @@ using FAB.Forms;
 
 namespace FAB.iOS
 {
-    public partial class FloatingActionButtonRenderer : ViewRenderer<FloatingActionButton, MNFloatingActionButton>
-    {
-        protected override void OnElementChanged(ElementChangedEventArgs<FloatingActionButton> e)
-        {
-            base.OnElementChanged(e);
+	public partial class FloatingActionButtonRenderer : ViewRenderer<FloatingActionButton, MNFloatingActionButton>
+	{
+		protected override void OnElementChanged(ElementChangedEventArgs<FloatingActionButton> e)
+		{
+			base.OnElementChanged(e);
 
-            if (e.NewElement != null)
-            {
-                var fab = new MNFloatingActionButton();
-                fab.Frame = new CoreGraphics.CGRect(0, 0, 24, 24);
+			if (this.Control == null)
+			{
+				var fab = new MNFloatingActionButton();
+				fab.Frame = new CoreGraphics.CGRect(0, 0, 24, 24);
 
-                this.SetNativeControl(fab);
+				this.SetNativeControl(fab);
 
-                this.UpdateStyle();
+				this.UpdateStyle();
+			}
 
-                this.Control.TouchCancel += this.Fab_TouchUpInside;
-            }
-        }
+			if (e.NewElement != null)
+			{
+				this.Control.TouchUpInside += this.Fab_TouchUpInside;
+			}
 
-        void Fab_TouchUpInside (object sender, EventArgs e)
-        {
-            this.Element.SendClicked();
-        }
+			if (e.OldElement != null)
+			{
+				this.Control.TouchUpInside -= this.Fab_TouchUpInside;
+			}
+		}
 
-        protected override void OnElementPropertyChanged(object sender, System.ComponentModel.PropertyChangedEventArgs e)
-        {
-            base.OnElementPropertyChanged(sender, e);
+		void Fab_TouchUpInside(object sender, EventArgs e)
+		{
+			this.Element.SendClicked();
+		}
 
-            if(e.PropertyName == "Width" ||
-                e.PropertyName == "Height" ||
-                e.PropertyName == "X" ||
-                e.PropertyName == "Y")
-                {
-                    
-                }
-        }
+		public override SizeRequest GetDesiredSize(double widthConstraint, double heightConstraint)
+		{
+			var viewSize = this.Element.Size == FAB.Forms.FabSize.Normal ? 56 : 40;
 
-        public override SizeRequest GetDesiredSize(double widthConstraint, double heightConstraint)
-        {
-            var viewSize = this.Element.Size == FAB.Forms.FabSize.Normal ? 56 : 40;
-            
-            return new SizeRequest(new Size(viewSize, viewSize)); //base.GetDesiredSize(widthConstraint, heightConstraint);
-        }
+			return new SizeRequest(new Size(viewSize, viewSize));
+		}
 
-        public override void LayoutSubviews()
-        {
-            base.LayoutSubviews();
-        }
+		private void UpdateStyle()
+		{
+			switch (this.Element.Size)
+			{
+			case FAB.Forms.FabSize.Mini:
+				this.Control.Size = MNFloatingActionButton.FABSize.Mini;
+				break;
+			case FAB.Forms.FabSize.Normal:
+				this.Control.Size = MNFloatingActionButton.FABSize.Normal;
+				break;
+			}
 
-        protected override void Dispose(bool disposing)
-        {
-            if (disposing)
-            {
-                this.Control.TouchCancel -= this.Fab_TouchUpInside;
-            }
+			this.Control.BackgroundColor = this.Element.NormalColor.ToUIColor();
+			this.Control.PressedBackgroundColor = this.Element.PressedColor.ToUIColor();
 
-            base.Dispose(disposing);
-        }
+			this.Control.HasShadow = this.Element.HasShadow;
 
-        private void UpdateStyle()
-        {
-            switch (this.Element.Size)
-            {
-                case FAB.Forms.FabSize.Mini:
-                    this.Control.Size = MNFloatingActionButton.FABSize.Mini;
-                    break;
-                case FAB.Forms.FabSize.Normal:
-                    this.Control.Size = MNFloatingActionButton.FABSize.Normal;
-                    break;
-            }
+			SetImageAsync(this.Element.Source, (float)this.Element.WidthRequest, (float)this.Element.Height, this.Control);
+		}
 
-            this.Control.BackgroundColor = this.Element.NormalColor.ToUIColor();
-            this.Control.PressedBackgroundColor = this.Element.PressedColor.ToUIColor();
-
-            this.Control.HasShadow = this.Element.HasShadow;
-
-            Task.Run(async () =>
-                {
-                    await SetImageAsync(this.Element.Source, (float)this.Element.WidthRequest, (float)this.Element.Height, this.Control);
-                });
-        }
-
-        private async static Task SetImageAsync(ImageSource source, nfloat widthRequest, nfloat heightRequest, MNFloatingActionButton targetButton)
-        {
-            var handler = GetHandler(source);
-            using (UIImage image = await handler.LoadImageAsync(source))
-            {
-                UIGraphics.BeginImageContext(new CoreGraphics.CGSize(widthRequest, heightRequest));
-                image.Draw(new CoreGraphics.CGRect(0, 0, widthRequest, heightRequest));
-                using (var resultImage = UIGraphics.GetImageFromCurrentImageContext())
-                {
-                    UIGraphics.EndImageContext();
-                    using (var resizableImage = resultImage.CreateResizableImage(new UIEdgeInsets(0f, 0f, widthRequest, heightRequest)))
-                    {
-                        targetButton.CenterImageView.Image = resizableImage.ImageWithRenderingMode(UIImageRenderingMode.AlwaysOriginal);
-                    }
-                }
-            }
-        }
-    }
+		private async static void SetImageAsync(ImageSource source, nfloat widthRequest, nfloat heightRequest, MNFloatingActionButton targetButton)
+		{
+			var handler = GetHandler(source);
+			using (UIImage image = await handler.LoadImageAsync(source))
+			{
+				UIGraphics.BeginImageContext(new CoreGraphics.CGSize(widthRequest, heightRequest));
+				image.Draw(new CoreGraphics.CGRect(0, 0, widthRequest, heightRequest));
+				using (var resultImage = UIGraphics.GetImageFromCurrentImageContext())
+				{
+					if (resultImage != null)
+					{
+						UIGraphics.EndImageContext();
+						using (var resizableImage = resultImage.CreateResizableImage(new UIEdgeInsets(0f, 0f, widthRequest, heightRequest)))
+						{
+							targetButton.CenterImageView.Image = resizableImage.ImageWithRenderingMode(UIImageRenderingMode.AlwaysOriginal);
+						}
+					}
+				}
+			}
+		}
+	}
 }
