8b3f624fd739333dc50a281e813a24ec0927864e
Drew Frisk <drew@drewfrisk.com>
2016-08-16 22:25:35 -0500

    added linker preserve files to hopefully address issue #3

---
+++ Library/FloatingActionButton.Droid/LinkerPleaseInclude.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Android.Runtime;
+using FAB.Droid;
+
+namespace FAB.iOS
+{
+    [Preserve(AllMembers = true)]
+    public class LinkerPleaseInclude
+    {
+        public void Include(FloatingActionButtonRenderer renderer)
+        {
+            var r = new FloatingActionButtonRenderer();
+        }
+    }
+}
---
+++ Library/FloatingActionButton.iOS/LinkerPleaseInclude.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Foundation;
+
+namespace FAB.iOS
+{
+    [Preserve(AllMembers = true)]
+    public class LinkerPleaseInclude
+    {
+        public void Include(FloatingActionButtonRenderer renderer)
+        {
+            var r = new FloatingActionButtonRenderer();
+        }
+    }
+}
